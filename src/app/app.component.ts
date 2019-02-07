53edde64e68c7d1741dd5156b51228123395da89
artem.horobei <artem.horobei@yalantis.com>
2019-02-07 15:52:21 +0200

    Add todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-TODO';
+  todoArray = [1, 2, 3];
+  isEdit = false;
+
+  handleToggleEditItem = () => this.isEdit = !this.isEdit;
+
+  addTodo = (value) => {
+    this.todoArray.push(value);
+  };
+
+  deleteTodo = (todo) => {
+    this.todoArray = this.todoArray.filter(item => item !== todo);
+  };
+
+  editTodo = (todo) => {
+    this.todoArray = this.todoArray.map(item => {
+      return item === todo ? todo : item;
+    });
+  };
 }
