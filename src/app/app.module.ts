e6579291e2a79c3e42e380c171ee9eb5e0565f0e
artem.horobei <artem.horobei@yalantis.com>
2019-02-08 17:50:55 +0200

    Add real request to users list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { VillainsComponent } from './villains/villains.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VillainsComponent,
-    HeroesComponent
+    UserComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/constants/config.js
@@ -0,0 +1,6 @@
+export const configAPI = {
+  baseUrl: 'https://jsonplaceholder.typicode.com/',
+  entities: {
+    users: 'users',
+  },
+};
